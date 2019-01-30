278ff6254a54187567db298e410db3b66a3bc3fb
a1iya <aliyusha.gizatullina@gmail.com>
2019-01-30 13:41:32 +0100

    user

--- blog-rails/db/seeds.rb
+++ blog-rails/db/seeds.rb
@@ -1,3 +1,6 @@
+User.create(name: "jean", email:"jean@jean.jean")
+User.create(name: "paul", email:"paul@paul.paul")
+p "Deux utilisateurs ont été créés"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
